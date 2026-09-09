66b09f3442e20a9d071a0889a1a944252d28db30
hindFrdus <hooo7080@gmail.com>
2026-09-09 23:04:13 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,31 @@
+
+
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAPntZX4tx4zFw5nyWDycc2PZZ3hAfxIJA",
+    authDomain: "game-b2314.firebaseapp.com",
+    databaseURL: "https://game-b2314-default-rtdb.firebaseio.com",
+    projectId: "game-b2314",
+    storageBucket: "game-b2314.firebasestorage.app",
+    messagingSenderId: "1079800887430",
+    appId: "1:1079800887430:web:337ce2a7a479418febf907",
+    measurementId: "G-FXWEB8QL3L"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
+
+
+
