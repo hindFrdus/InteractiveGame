5cc2a432382a87581399e7f1cb6e294013f792c5
hindFrdus <hooo7080@gmail.com>
2026-09-11 02:17:39 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,17 +10,16 @@
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAPntZX4tx4zFw5nyWDycc2PZZ3hAfxIJA",
-    authDomain: "game-b2314.firebaseapp.com",
-    databaseURL: "https://game-b2314-default-rtdb.firebaseio.com",
-    projectId: "game-b2314",
-    storageBucket: "game-b2314.firebasestorage.app",
-    messagingSenderId: "1079800887430",
-    appId: "1:1079800887430:web:337ce2a7a479418febf907",
-    measurementId: "G-FXWEB8QL3L"
-  };
-
+ const firebaseConfig = {
+  apiKey: "AIzaSyAPntZX4tx4zFw5nyWDycc2PZZ3hAfxIJA",
+  authDomain: "game-b2314.firebaseapp.com",
+  databaseURL: "https://game-b2314-default-rtdb.firebaseio.com",
+  projectId: "game-b2314",
+  storageBucket: "game-b2314.firebasestorage.app",
+  messagingSenderId: "1079800887430",
+  appId: "1:1079800887430:web:337ce2a7a479418febf907",
+  measurementId: "G-FXWEB8QL3L"
+};
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
